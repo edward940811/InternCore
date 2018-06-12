852907af5f7e514cdc958b31cbc97b7ea76bf61c
Kyle Shen <kyle.shen616@gmail.com>
2018-06-12 13:51:42 +0800

    WishNotification Core

---
+++ Utility.WishNotification/SlackNotification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Utility.WishNotification
+{
+    public class SlackNotification : INotification
+    {
+        public void Send(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
